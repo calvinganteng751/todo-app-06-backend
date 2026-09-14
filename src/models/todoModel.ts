2f522a56615adb2cfc310072452933b16dee83b4
calvin-mobile <calvinganteng751@example.com>
2026-09-14 11:58:07 +0700

    Tugas 5: tambah update, delete, getById, dan rapikan routing

--- src/models/todoModel.ts
+++ src/models/todoModel.ts
@@ -15,4 +15,36 @@ export const TodoModel = {
     );
     return result.insertId;
   },
+
+  // Update task atau status is_completed
+  update: async (
+    id: number,
+    task: string,
+    isCompleted: boolean,
+    userId: number,
+  ) => {
+    const [result]: any = await pool.query(
+      "UPDATE todos SET task = ?, is_completed = ? WHERE id = ? AND user_id = ?",
+      [task, isCompleted, id, userId],
+    );
+    return result.affectedRows;
+  },
+
+  // Hapus todo berdasarkan id dan userId
+  delete: async (id: number, userId: number) => {
+    const [result]: any = await pool.query(
+      "DELETE FROM todos WHERE id = ? AND user_id = ?",
+      [id, userId],
+    );
+    return result.affectedRows;
+  },
+
+  // (Langkah 10) Ambil 1 todo berdasarkan id, pastikan miliknya user ini
+  getById: async (id: number, userId: number) => {
+    const [rows]: any = await pool.query(
+      "SELECT * FROM todos WHERE id = ? AND user_id = ?",
+      [id, userId],
+    );
+    return rows[0]; // Kembalikan 1 data, atau undefined jika tidak ditemukan
+  },
 };
